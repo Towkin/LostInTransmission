280ec38690465d9c83efe012ac87ea9086ef4f03
Emanuel Strömgren <emanuel.stromgren@pulpeten.se>
2018-01-28 03:00:00 +0100

    Uh, well, here's the actual stuff as well.

---
+++ LostInTransmissionUnity/Assets/_Master/Scripts/MessageOptionDropdown.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MessageOptionDropdown : MonoBehaviour
+{
+    public int m_SelectOptionIndex;
+    public int m_OptionSetIndex;
+    public RouterController m_Router;
+
+    public void SelectOption()
+    {
+        m_Router.SetOptions(m_OptionSetIndex, m_SelectOptionIndex);
+    }
+
+    public void Activate()
+    {
+        gameObject.SetActive(true);
+    }
+}
