ec9b58fb706c9eef74a27d0a27fbaaaa8f370ece
Hedman <jocke_hedman@hotmail.com>
2018-01-28 07:55:59 +0100

    animation of dialogue menu

---
+++ LostInTransmissionUnity/Assets/_Master/Scripts/DialogueSmooth.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueSmooth : MonoBehaviour {
+
+    private Animator m_Anim;
+
+    void Start()
+    {
+        m_Anim = GetComponent<Animator>();
+
+        //m_Anim.SetBool("EnableSmooth", true);
+    }
+
+
+    //public void OnPointerEnter(PointerEventData eventData)
+    //{
+
+
+    //}
+
+    //public void OnPointerExit(PointerEventData eventData)
+    //{
+    //    m_Anim.SetBool("EnableSmooth", false);
+    //}
+}
