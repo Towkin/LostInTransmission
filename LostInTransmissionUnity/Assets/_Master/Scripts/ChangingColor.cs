81ff380651b3797825f5d3c1ae4ca412e4a0bf6e
Hedman <jocke_hedman@hotmail.com>
2018-01-28 10:37:31 +0100

    changing color title, kinda weird. some animation for dialogue grej

---
+++ LostInTransmissionUnity/Assets/_Master/Scripts/ChangingColor.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangingColor : MonoBehaviour {
+
+    float timeLeft;
+    Color targetColor;
+
+    Text m_TitleText;
+
+    private void Start()
+    {
+        m_TitleText = GetComponent<Text>();
+    }
+
+    void Update()
+    {
+        if (timeLeft <= Time.deltaTime)
+        {
+            m_TitleText.color = targetColor;
+            targetColor = new Color(Random.value, Random.value, Random.value);
+            timeLeft = 1.0f;
+        }
+        else
+        {
+            m_TitleText.color = Color.Lerp(m_TitleText.color, targetColor, Time.deltaTime / timeLeft);
+            timeLeft -= Time.deltaTime;
+        }
+    }
+}
