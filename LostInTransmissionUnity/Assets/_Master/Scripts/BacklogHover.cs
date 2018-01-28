f51a801174c348bbc69443727541ad684e87f0da
Hedman <jocke_hedman@hotmail.com>
2018-01-28 03:05:57 +0100

    Backlog hover thingy

---
+++ LostInTransmissionUnity/Assets/_Master/Scripts/BacklogHover.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class BacklogHover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler {
+
+    private Animator m_Anim;
+
+    void Start()
+    {
+        m_Anim = GetComponent<Animator>();
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        m_Anim.SetBool("EnableHover", true);
+        
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        m_Anim.SetBool("EnableHover", false);
+    }
+}
